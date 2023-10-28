ec0bc6de9d106ff0237b3ee3c6ba5c22e1a3ba73
david waza <idemetoemediong2022@gmail.com>
2023-10-28 08:51:31 +0100

    added the cirles on desktop

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="home-bg w-full px-4 lg:px-[12rem] pt-20">
+    <div className="body-bg w-full px-4 lg:px-[12rem] pt-20">
       <Box sx={{ paddingTop: 20, flexGrow: 1 }}>
         <Grid container spacing={5}>
           <Grid item xs={12} md={12} lg={6}>
@@ -15,16 +15,17 @@ const Hero = () => {
               Next level funding
             </p>
             <p className="text-white space-grotesk font-bold text-[1.8rem] text-center lg:text-[4.5rem] lg:text-left md:leading-[73px] leading-[39px]">
-              Our Capital, Our Risk, <span className="text-blue-500">Your Knowledge.</span>
+              Our Capital, Our Risk,{" "}
+              <span className="text-blue-500">Your Knowledge.</span>
             </p>
             <div className="md:w-[30rem] m-auto lg:ml-0">
-            <p className="text-sm xl:text-lg tracking-wide md:text-left text-center font-light lexend text-white 2xl:w-[35rem] mt-1 ">
-              With our program, you can get funded up to £1,000,000 with up to a
-              90% profit split. Unlock Your Trading Potential with Our Leading
-              Proprietary Trading Firm.
-            </p>
+              <p className="text-sm xl:text-lg tracking-wide md:text-left text-center font-light lexend text-white 2xl:w-[35rem] mt-1 ">
+                With our program, you can get funded up to £1,000,000 with up to
+                a 90% profit split. Unlock Your Trading Potential with Our
+                Leading Proprietary Trading Firm.
+              </p>
             </div>
-            
+
             <div className="lg:flex gap-4 mt-7 grid justify-center lg:justify-start">
               <Link href="/about-us">
                 <Button
@@ -42,12 +43,19 @@ const Hero = () => {
             </div>
           </Grid>
           <Grid item xs={12} md={12} lg={6}>
-            <div className="flex xl:justify-end justify-center">
-              <img
-                src="/assets/laptop.gif"
-                className="lg:w-[30rem] w-[20rem] h-auto"
-                alt="trade-computer"
-              />
+            <div className="flex xl:justify-end justify-center relative">
+              <div className="bg-white rounded-full h-20 w-20 p-2 backdrop-filter bg-[transparent] backdrop-blur-lg bg-opacity-30">
+                <img src="/assets/eth.png" className="" alt="eth" />
+              </div>
+              <div className="backdrop-filter bg-[transparent] backdrop-blur-lg bg-opacity-30 w-30 h-30 rounded-full absolute p-[5rem] left-[40%] bottom-[30%] border border-slate-800 bg-white hidden lg:block"></div>
+              <div className="relative z-30">
+                <img
+                  src="/assets/laptop.gif"
+                  className="lg:w-[30rem] w-[20rem] h-auto"
+                  alt="trade-computer"
+                />
+              </div>
+              <div className="backdrop-filter bg-[transparent] backdrop-blur-lg bg-opacity-30 w-30 h-30 rounded-full absolute p-[5rem] left-[60%] bottom-[10%] border border-slate-800 bg-white hidden lg:block"></div>
             </div>
           </Grid>
         </Grid>
